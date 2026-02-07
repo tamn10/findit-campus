3c8edfd4a09af1d8aa0d8e390e04d7bed60ad094
tAmh <ttrhuynh07@gmail.com>
2026-02-06 23:46:27 -0800

    Added welcome screen UI

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,16 @@
+import { router } from "expo-router";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LoginScreen() {
+  return (
+    <SafeAreaView className="flex-1 bg-lime-300">
+      <Pressable onPress={() => router.back()} className="p-4">
+        <Text className="text-xl">← Go Back</Text>
+      </Pressable>
+      <View className="flex-1 items-center justify-center">
+        <Text>Login Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
